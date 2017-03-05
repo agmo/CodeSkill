6ca22f90dee39d107a69f910dda2bc3667718bf8
agmo <mor.agnieszka@gmail.com>
2017-03-05 16:12:14 +0100

    Account for large numbers

--- reverseDigits.js
+++ reverseDigits.js
@@ -1,20 +1,31 @@
 function reverseDigits(number) {
     var negative = number < 0;
     var stringified = Math.abs(number).toString();
-    var result = stringified
+    var stringResult = stringified
         .split('')
         .reverse()
         .join('')
         .trimLeadingZeros();
 
-    return negative ? Number('-' + result) : Number(result);
+    var numberResult = Number(stringResult);
+
+    if (String(numberResult) === stringResult) {
+        return negative ? (numberResult * -1) : numberResult;
+    } else {
+        throw new Error('The converted number cannot be accurately represented.')
+    }
+
 }
 
 ///////////////////////////////////////////////////
 (function () {
     if (typeof String.prototype.trimLeadingZeros !== 'function') {
         String.prototype.trimLeadingZeros = function () {
-            var trimmedString = this;
+            var trimmedString = this.valueOf();
+
+            if (trimmedString.length === 1 && trimmedString.charAt(0) === '0') { //Do not trim for zero
+                return trimmedString;
+            }
 
             while (trimmedString.charAt(0) === '0') {
                 trimmedString = trimmedString.substr(1);
