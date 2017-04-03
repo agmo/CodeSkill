c269c6ed7b0ca4499e26c281c24ec1a8355c5b5f
agmo <mor.agnieszka@gmail.com>
2017-04-03 23:22:09 +0200

    Fix a false value being converted to zero

--- sumPairs.js
+++ sumPairs.js
@@ -2,8 +2,29 @@ function sumPairs(array) {
     var result = [];
 
     for (var i = 0; i < array.length; i += 2) {
-        var value1  = Number.isSafeInteger(array[i]) && array[i];
-        var value2 = typeof array[i + 1] !== 'undefined' ? (Number.isSafeInteger(array[i+1])) && array[i + 1] : 0;
+        var value1  = Number.isSafeInteger(array[i]);
+        var value2;
+
+        if (!value1) {
+            result.push('skipped');
+            continue;
+        }
+
+        value1 = array[i];
+
+        if (typeof array[i + 1] !== 'undefined') {
+            value2 = Number.isSafeInteger(array[i + 1]);
+
+            if (!value2) {
+                result.push('skipped');
+                continue;
+            }
+
+            value2 = array[i + 1];
+        } else {
+            value2 = 0;
+        }
+
         var pairTotal =  Number.isSafeInteger(value1 + value2) && (value1 + value2);
 
         if (pairTotal) {
